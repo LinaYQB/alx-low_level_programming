5e79829757fccc8755993b31534429279c8797f6
Lina <yqblina@gmail.com>
2023-03-16 14:23:06 -0700

    correction 2 task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main -Entry point
  *
- * Description: c programme using puts function
- * 
- * Return: always 0
- */
+ * Description: a C program using puts function
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
