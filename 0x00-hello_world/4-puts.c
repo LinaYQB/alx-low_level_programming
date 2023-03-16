9d4d8e2dde781ea1fc4cb44fac763fa7282c9c1d
Lina <yqblina@gmail.com>
2023-03-16 14:04:15 -0700

    Task 4 commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - entry point
+ * description : c programe using puts function 
+ * return: always 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return(0);
+}
