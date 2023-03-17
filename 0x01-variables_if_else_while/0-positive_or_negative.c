ec75385e83e5fa23598eb448fa4cab77647cf5d2
Lina <yqblina@gmail.com>
2023-03-17 13:06:29 -0700

    Task 0 commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: c program generating random number and sorting it
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+	{
+		printf("%u is negative\n", n);
+	}
+	else
+	{
+		if (n == 0)
+		{
+			printf("%u is zero\n", n);
+		}
+		else
+			printf("%u is positive\n", n);
+	}
+	return (0);
+}
