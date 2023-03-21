83b7513d5898e5606fe8ba596d9d2156e6720738
Lina <yqblina@gmail.com>
2023-03-21 14:52:48 -0700

    Task 0 commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program taht prints _putchar
+ *
+ * Return: always 0 (Success)
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
