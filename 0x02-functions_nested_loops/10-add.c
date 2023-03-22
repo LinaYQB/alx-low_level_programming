79861c508a455cec247d6a9a3b8e3da6a8cb8faf
Lina <yqblina@gmail.com>
2023-03-21 17:22:48 -0700

    Task 10 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @a: firt integer
+ *
+ * @b: seconde integer to be added
+ *
+ * Descriptin: func that adds two integers and returns the result
+ *
+ * Return: the result
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
