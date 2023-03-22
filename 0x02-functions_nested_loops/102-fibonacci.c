9191a4ef2bd8d7e42810938a99494e3ee31aadbd
Lina <yqblina@gmail.com>
2023-03-21 18:33:23 -0700

    Task 14 correction

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,18 +10,20 @@
 int main(void)
 {
 	int i;
-	int tab[50];
+	unsigned long long tab[50];
 
 	tab[0] = 1;
 	tab[1] = 2;
 
 	for (i = 2; i < 50; i++)
 	{
-		tab[i] = tab[i - 1] - tab[i - 2];
+		tab[i] = tab[i - 1] + tab[i - 2];
 	}
 	for (i = 0; i < 50; i++)
 	{
-		printf("%d, ", tab[i]);
+		printf("%llu", tab[i]);
+		if (i != 49)
+			printf(", ");
 	}
 	printf("\n");
 	return (0);
