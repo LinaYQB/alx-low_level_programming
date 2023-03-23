dad0c5b99f0ce38e320c24a03658e58eae11d2c2
Lina <yqblina@gmail.com>
2023-03-23 14:55:54 -0700

    Task 4 commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 01356789
+ *
+ * no retun
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if ((i == 50) || (i == 52))
+			continue;
+		_putchar(i);
+	}
+	_putchar('\n');
+}
