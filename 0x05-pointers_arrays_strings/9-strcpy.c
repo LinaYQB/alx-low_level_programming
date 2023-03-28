9712a32c3930ffa61e0b6086e3396913257888df
Lina <yqblina@gmail.com>
2023-03-27 18:58:06 -0700

    Task 9 commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src to dest
+ * @src: the string to copy
+ * @dest: the string returned
+ * Return: 0 success
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] <= '\0'; i++)
+		dest[i] = src[i];
+	return (dest);
+}
