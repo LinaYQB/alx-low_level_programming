c8fc7246e32c76d3677da23b27d78db005be80ee
Lina <yqblina@gmail.com>
2023-04-11 15:29:30 -0700

    Task 0 commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of array
+ * @c: char to initialize
+ * Return: pointer to char
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *a = malloc(size);
+
+	if (size == 0 || n == 0)
+		return (0);
+
+	while (size--)
+		a[size] = c;
+
+	return (a);
+}
