a4ff7db4f012e62ff59aba88caa3c0eadb577428
Lina <yqblina@gmail.com>
2023-05-02 16:20:44 -0700

    Task 13 commit co

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,6 +1,27 @@
 #include "lists.h"
 
 /**
+ * find_listint_loop1 - finds the loop in a linked list
+ * @head: linked list to search in
+ * Return: address of node where the loop starts
+*/
+listint_t *find_listint_loop1(listint_t *head)
+{
+	listint_t *p, *end;
+
+	if (head == NULL)
+		return (NULL);
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end == end->next)
+			return (end);
+		for (p = head; p != end; p = p->next)
+			if (p == end->next)
+				return (end->next);
+	}
+	return (NULL);
+}
+/**
  * free_listint_safe - frees a listint list
  * @h: head of list
  * Return: size of the list freed
@@ -14,7 +35,7 @@ size_t free_listint_safe(listint_t **h)
 	if (h == NULL || *h == NULL)
 		return (0);
 
-	loopnode = find_listint_loop(&(*h));
+	loopnode = find_listint_loop1(*h);
 	for (i = 0; (*h != loopnode || lp) && *h != NULL; *h = next)
 	{
 		i++;
