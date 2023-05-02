d14f6985793a2a5dc5360a075e0cbc9d4267a4f3
Lina <yqblina@gmail.com>
2023-05-02 15:56:37 -0700

    Task 13 commit

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a listint list
+ * @h: head of list
+ * Return: size of the list freed
+*/
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *next, *loopnode;
+	size_t i;
+	int lp = 1;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	loopnode = find_listint_loop(*h);
+	for (i = 0; (*h != loopnode || lp) && *h != NULL; *h = next)
+	{
+		i++;
+		next = (*h)->next;
+		if (*h == loopnode && lp)
+		{
+			if (loopnode == loopnode->next)
+			{
+				free(*h);
+				break;
+			}
+			i++;
+			next = next->next;
+			free((*h)->next);
+			loop = 0;
+		}
+		free(*h);
+	}
+	*h = NULL;
+	return (i);
+}
